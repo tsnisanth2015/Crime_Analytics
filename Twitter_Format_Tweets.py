100e98042812363e09aa0b713dd3a958c84b3d94
Nisanth TS <nisanth.ts@gmail.com>
2017-10-23 02:33:07 +0530

    Update Twitter_Format_Tweets.py

--- Twitter_Format_Tweets.py
+++ Twitter_Format_Tweets.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-tweets_data_path = 'd://DataFiles_Backup//bangalore_bk_3K.txt'
+tweets_data_path = 'd://DataFiles_Backup//bangalore_bk_8K.txt'
 
 tweets_data = []
 tweets_file = open(tweets_data_path,"r")
@@ -15,13 +15,14 @@ for line in tweets_file:
         continue
 print len(tweets_data)
 tweets = pd.DataFrame()
-tweets['text'] = map(lambda tweet: tweet['text'],tweets_data)
-tweets['lang'] = map(lambda tweet: tweet['lang'],tweets_data)
-tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place']!= None else None,tweets_data)
-
-
-
-#tweets.to_csv("d://DataFiles_Backup//bangalore_3k.csv", encoding='utf-8')
+tweets['Tweet_Text'] = map(lambda tweet: tweet['text'],tweets_data)
+tweets['Time_Of_Tweet'] =  map(lambda tweet: tweet['created_at'],tweets_data)
+tweets['Tweet_Location'] =  map(lambda tweet: tweet['place']['name'],tweets_data)
+tweets['User_Native'] = map(lambda tweet: tweet['user']['location'],tweets_data)
+tweets['Country'] = map(lambda tweet: tweet['place']['country'] if tweet['place']!= None else None,tweets_data)
+tweets['Language'] = map(lambda tweet: tweet['lang'],tweets_data)
+    
+tweets.to_csv("d://DataFiles_Backup//bangalore_8k.csv", encoding='utf-8')
 
 
 ##tweets_by_lang = tweets['lang'].value_counts()
@@ -33,5 +34,3 @@ tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'
 ##ax.set_ylabel('Number of tweets' , fontsize=15)
 ##ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
 ##tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
-
-
